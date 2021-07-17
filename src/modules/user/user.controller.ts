57f2be4682ba65e6a76605d3216d669c4ffaeb23
Boris Šuličenko <b.sulicenko@gmail.com>
2021-07-17 17:55:28 +0200

    #3

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards, Request, Param } from "@nestjs/common";
+import { Controller, Get, UseGuards, Request, Param, Post, Body } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { UserService } from "./user.service";
+import { UserRegistrationDto } from "./user.types";
 
 @Controller('user/user')
 export class UserController {
@@ -15,5 +16,10 @@ export class UserController {
     @Get('profile/:username')
     async getProfile(@Param('username') username: string) {
         return this.userService.findByUsername(username)
+    }
+
+    @Post('register')
+    async register(@Body() payload: UserRegistrationDto) {
+        return this.userService.create(payload)
     }
 }
